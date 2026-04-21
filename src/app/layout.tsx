24d4a87320386831ff42d37fb192d464755f8725
dk <kudin.dima@gmail.com>
2026-04-21 22:27:58 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,54 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import Tag from "@/tag/Tag";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { DM_Sans } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'Kinza Graphic Designer | Professional Branding & Design Services',
+  description: 'Professional graphic design services for local businesses. Specializing in logo design, branding, and high-impact visual content. Get a free quote today.',
+  keywords: ["graphic designer, logo design, branding services, local graphic designer, visual identity"],
+  openGraph: {
+    "title": "Kinza Graphic Designer",
+    "description": "Professional Branding & Design Services for Local Businesses.",
+    "siteName": "Kinza Graphic Designer",
+    "type": "website"
+  },
+};
+
+const dmSans = DM_Sans({
+  variable: "--font-dm-sans",
+  subsets: ["latin"],
+});
+const inter = Inter({
+  variable: "--font-inter",
+  subsets: ["latin"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${dmSans.variable} ${inter.variable} antialiased`}>
+          <Tag />
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
